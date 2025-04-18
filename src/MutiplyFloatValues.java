dc6b68532dc50770f8a9ec91be3bddc0fc829313
sravyamurukutla <murukutlasravya@gmail.com>
2025-04-18 10:54:49 -0400

    "Multiply two float variables"

---
+++ src/MutiplyFloatValues.java
@@ -0,0 +1,11 @@
+public class MutiplyFloatValues {
+
+    public static void main(String[] args) {
+        float f1 = 3.5f;
+        float f2 = 5.5f;
+        float f3 = f1 * f2;
+
+        System.out.println(f3);
+
+    }
+}
